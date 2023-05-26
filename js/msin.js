05712f4ba346294399a22d25fd1ff400b24624c4
prabeshsitaula07 <sitaulaprabesh07@gmail.com>
2023-05-26 13:03:06 +0545

    initial commit

---
+++ js/msin.js
@@ -0,0 +1,43 @@
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+// counter design
+ document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration /  range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 127, 3000);
+    counter("count2", 100, 86, 2500);
+    counter("count3", 0, 140, 3000);
+    counter("count4", 0, 710, 3000);
+ });
+   
+   
